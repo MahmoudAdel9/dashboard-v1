ea13e27c9f5fcbe83b8558f5ff1866b0226389a5
mahmoud adel <mahmoud.adel5375474@gmail.com>
2023-11-02 01:18:24 +0200

    update

--- src/layouts/dashboard/common/DarkModeToggle.jsx
+++ src/layouts/dashboard/common/DarkModeToggle.jsx
@@ -11,7 +11,7 @@ function DarkModeToggle() {
   return (
     <div>
       <IconButton onClick={toggleMode}>
-        <SvgIcon src={imagePath} />
+        <img alt="dark-mode" src={imagePath} width={25} height={25} />
       </IconButton>
     </div>
   );
