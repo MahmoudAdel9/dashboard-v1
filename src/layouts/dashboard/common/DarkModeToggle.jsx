14769876ea4936fc580d89ca864a09da9d338590
mahmoud adel <mahmoud.adel5375474@gmail.com>
2023-11-02 16:48:47 +0200

    update

--- src/layouts/dashboard/common/DarkModeToggle.jsx
+++ src/layouts/dashboard/common/DarkModeToggle.jsx
@@ -4,13 +4,13 @@ import { useDarkMode } from 'src/theme/DarkModeContext';
 
 function DarkModeToggle() {
   const { isDarkMode, toggleMode } = useDarkMode();
-  const imagePath = isDarkMode
-    ? '/public/assets/icons/navbar/sun.svg'
-    : '/public/assets/icons/navbar/moon.svg';
+  // const imagePath = isDarkMode
+  //   ? '/public/assets/icons/navbar/sun.svg'
+  //   : '/public/assets/icons/navbar/moon.svg';
   return (
     <div>
       <IconButton onClick={toggleMode}>
-        <Box component="img" alt="dark-mode" src={imagePath} sx={{ width: 25, height: 25 }} />
+        <img src="/public/assets/icons/navbar/sun.svg" alt="dark-mode" width={25} height={25} />
       </IconButton>
     </div>
   );
